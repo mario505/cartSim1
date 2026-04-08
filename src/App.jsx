61ce1585e151b439dbca2d53d22dd9d55836d548
mario <mario_reyes505@hotmail.com>
2026-04-07 22:15:31 -0500

    delete AppOld.jsx. create store/shopping-cart-context.jsx, add it to App.jsx. Refactor Shop.jsx to receive children instead of prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-
-import Header from './components/Header.jsx';
-import Shop from './components/Shop.jsx';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
+import { useState } from "react";
+import { CartContext } from "./store/shopping-cart-context.jsx";
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import Product from "./components/Product.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -14,7 +15,7 @@ function App() {
       const updatedItems = [...prevShoppingCart.items];
 
       const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
+        (cartItem) => cartItem.id === id,
       );
       const existingCartItem = updatedItems[existingCartItemIndex];
 
@@ -44,7 +45,7 @@ function App() {
     setShoppingCart((prevShoppingCart) => {
       const updatedItems = [...prevShoppingCart.items];
       const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
+        (item) => item.id === productId,
       );
 
       const updatedItem = {
@@ -66,13 +67,19 @@ function App() {
   }
 
   return (
-    <>
+    <CartContext>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
+    </CartContext>
   );
 }
 
